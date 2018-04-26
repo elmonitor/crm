b1b04e34b5edff0af028c07d4afcf212307bf4c3
elmonitor <info@elmonitor.net>
2018-04-26 12:17:28 -0500

    pet_weight

--- crm_lead_website/__manifest__.py
+++ crm_lead_website/__manifest__.py
@@ -4,8 +4,8 @@
 # Copyright 2017 David Vidal <david.vidal@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Website in leads",
-    "summary": "Add Website field to leads",
+    "name": "Pet Weight in leads",
+    "summary": "Add pet weight field to leads",
     "version": "10.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "https://www.tecnativa.com",
